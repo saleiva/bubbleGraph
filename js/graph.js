ee50498fa519cd0dbd091b22c92104e3fa7a3cc1
saleiva <saleiva@gmail.com>
2011-04-28 17:47:43 +0200

    first commit

---
+++ js/graph.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  
+  //draw a bubble
+  $('#container').append('<div class="bubbleContainer"><div class="outerBubble"></div><div class="innerBubble"></div></div>');
+  
+});
